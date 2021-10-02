f3dbdb4eb9e3d7e008afe85dd2cde1485cd275ff
masoud_mohebali <masoud_mohebali@yahoo.com>
2021-10-02 12:41:25 +0330

    imdb model db added

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # second apps
     'users',
+    'imdb',
 ]
 
 MIDDLEWARE = [
--- imdb/models.py
+++ imdb/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from config.users.models import User
 
 # Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+
+
+class Movie(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    director = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+    def __str__(self):
+        return f'{self.name}------\t{self.director}'
+
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    body = models.TextField()
+    rate = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
